f59960878c2de386dc222e5df22ef7e683bdfd38
RianAP <rian_ilkom54@apps.ipb.ac.id>
2019-01-29 15:43:00 +0700

    Perubahan alur algoritma di navigasi

--- navigasifix.ino
+++ navigasifix.ino
@@ -10,51 +10,78 @@
 short pulang[10000];
 short maze[10000];
 
-void setup() {
+void setup(){
   maze[0]=9;
 }
 
-void loop() {
-  mapping(9,0);
+void loop(){
+	mapping(9,1);
 }
-int x=0;
+int x=1;
 int mapping(int a, int b){
-  if(isFireAround()){
-      putFire();
-      return 1; 
-  }
-  if(abs(a-maze[b-1])=2) return 0;
-  if(a==1){
-	if(isNorthOpen()){
-		moveNorth();
-	}  
-  }
-  if(a==2){
-	if(isEastOpen()){
-		moveEast();
+	if(isFireAround()){
+		putFire();
+		return 1; 
 	}
-  }
-  if(a==3){
-	if(isSouthOpen()){
-		moveSouth();
+	if(abs(a-maze[b-1])=2) return 0;
+	if(a==1){
+		if(isNorthOpen()){
+			moveNorth();
+		}  
 	}
-  }
-  if(a==4){
-	if(isWestOpen()){
-		moveWest();
+	if(a==2){
+		if(isEastOpen()){
+			moveEast();
+		}
 	}
-  }
-  x++;
-  maze[x]=a;
-  if(mapping(1,x)==1) return 1;
-  if(mapping(2,x)==1) return 1;
-  if(mapping(3,x)==1) return 1;
-  if(mapping(4,x)==1) return 1;
-  maze[x]=9;
-  move(abs(a-2));
-  return 0;
+	if(a==3){
+		if(isSouthOpen()){
+			moveSouth();
+		}
+	}
+	if(a==4){
+		if(isWestOpen()){
+			moveWest();
+		}
+	}
+	maze[x]=a;
+	x++;
+	if(mapping(1,x)==1) return 1;
+	if(mapping(2,x)==1) return 1;
+	if(mapping(3,x)==1) return 1;
+	if(mapping(4,x)==1) return 1;
+	maze[x]=9;
+	move(abs(a-2));
+	return 0;
 }
 
+void move(int a){
+	if(a==1){
+		moveNorth();
+	}
+	if(a==2){
+		moveEast();
+	}
+	if(a==3){
+		moveSouth();
+	}
+	if(a==4){
+		moveWest();
+	}
+}
+
+void moveNorth(){
+  
+}
+void moveWest(){
+
+}
+void moveSouth(){
+   
+}
+void moveEast(){
+  
+}
 void putFire(){
   
 }
@@ -83,30 +110,4 @@ boolean isSouthOpen(){
 
 boolean isEastOpen(){
   
-}
-void moveNorth(){
-  
-}
-void moveWest(){
-
-}
-void moveSouth(){
-   
-}
-void moveEast(){
-  
-}
-void move(int a){
-	if(a==1){
-		moveNorth();
-	}
-	if(a==2){
-		moveEast();
-	}
-	if(a==3){
-		moveSouth();
-	}
-	if(a==4){
-		moveWest();
-	}
 }
